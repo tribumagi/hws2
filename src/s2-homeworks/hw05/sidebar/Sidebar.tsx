ea08b5e01b7775c2a37ea0f5f2de4d14494a0c08
Murad <djamavov.murad@gmail.com>
2023-02-01 21:45:41 +0300

    decorate v1.0

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -3,85 +3,19 @@ import {NavLink} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
 import closeIcon from './closeOutline.svg'
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 type PropsType = {
     open: boolean
     handleClose: () => void
 }
 
+
+
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
-
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-
-                        </IconButton>
-
-                            <nav id={'hw5-menu'} className={s.nav}>
-                                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                <NavLink
-                                    id={'hw5-pre-junior-link'}
-                                    to={PATH.PRE_JUNIOR}
-                                    onClick={handleClose}
-                                    // className={...} // делает студент
-                                >
-                                    Pre-junior
-                                </NavLink>
-                                </Typography>
-                                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                <NavLink
-                                    id={'hw5-junior-link'}
-                                    to={PATH.JUNIOR}
-                                    onClick={handleClose}
-                                    // className={...} // делает студент
-                                >
-                                    Junior
-                                </NavLink>
-                                </Typography>
-                                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                <NavLink
-                                    id={'hw5-junior-plus-link'}
-                                    to={PATH.JUNIOR_PLUS}
-                                    onClick={handleClose}
-                                    // className={...} // делает студент
-                                >
-                                    Junior Plus
-                                </NavLink>
-                                    </Typography>
-                            </nav>
-
-                        <Button color="inherit">Login</Button>
-
-                    </Toolbar>
-                </AppBar>
+< >
 
 
             {/*затемнение справа от открытого меню*/}
@@ -96,8 +30,36 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                     />
                 </button>
 
+                <nav id={'hw5-menu'} className={s.nav}>
+                <NavLink
+                    id={'hw5-pre-junior-link'}
+                    to={PATH.PRE_JUNIOR}
+                    onClick={handleClose}
+                    // className={...} // делает студент
+                >
+                    Pre-junior
+                </NavLink>
 
+                    <NavLink
+                        id={'hw5-junior-link'}
+                        to={PATH.JUNIOR}
+                        onClick={handleClose}
+                        // className={...} // делает студент
+                    >
+                        Junior
+                    </NavLink>
+
+                    <NavLink
+                        id={'hw5-junior-plus-link'}
+                        to={PATH.JUNIOR_PLUS}
+                        onClick={handleClose}
+                        // className={...} // делает студент
+                    >
+                        Junior Plus
+                    </NavLink>
+                </nav>
             </aside>
-            </Box>
+    </>
+
     )
 }
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return action.payload === 'up'
+                ? [...state].sort(((a,b) => a.name > b.name ? 1 : -1 ))
+                : [...state].sort(((a,b) => b.name > a.name ? 1 : -1 ))
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age > action.payload)
         }
         default:
             return state
