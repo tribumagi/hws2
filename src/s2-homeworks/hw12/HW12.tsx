54fbebe1994cf3bf2300f96ccd2c5ffefcb5f151
Murad <djamavov.murad@gmail.com>
2023-03-23 19:01:05 +0300

    h212 fix2

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -26,7 +26,7 @@ const HW12 = () => {
 
     const themeId=useSelector<AppStoreType, number>(state => state.theme.themeId)
     const dispatch=useDispatch()
-    const change = (id: any) => { // дописать функцию
+    const change = (id: number) => { // дописать функцию
         dispatch(changeThemeId(id))
     }
 
